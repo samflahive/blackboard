0e3db37e5db4ce5fdf7c31d6a3ec5e139b27ba2e
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-06-01 13:30:21 +0200

    added number class, sister class of math_blocks. random_number source also

---
+++ numbers.py
@@ -0,0 +1,40 @@
+import math_blocks
+from random import randint
+from scribble import Scribble
+
+class random_number:
+    def __init__(self, lower, upper, board=None, log=True):
+        if board == None:
+            raise ValueError("random_number objects require a math_board")
+        self.board = board
+
+        if log:
+            board.log(f"{upper}~{lower}|rn|_i_")
+        
+        self.upper = upper
+        self.lower = lower
+
+        self.math_board_id = self.board.track()
+
+    def generate(self):
+        return randint(self.lower, self.upper)
+
+
+
+
+class number(Scribble):
+    def __init__(self, value, sign=True, board=None, log=True):
+        if not isinstance(value, random_number):
+            raise ValueError("math_board numbers need a random_number object as its value")
+
+        if board == None:
+            board = value.board
+
+        if log:
+            board_message = f"{value.math_board_id}~{'T' if sign else 'F'}|n|_i_"
+        else:
+            board_message = None
+
+        block = math_blocks.number(value=value.generate(), sign=sign)
+
+        Scribble.__init__(self, block=block, board=board, message=board_message)
